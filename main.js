49a9debfd47af52e7ec3cfaf8e3024db4fd59f03
Abhishek-YadavSD <124059750+Abhishek-YadavSD@users.noreply.github.com>
2023-10-10 01:34:25 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,90 @@
+import { initScrollReveal } from "./scrollReveal.js";
+import { hoverChangeExperience } from "./hoverChangeExperience.js";
+import { typeWrite } from "./typeWrite.js";
+import { hoverChangeDescription } from "./hoverChangeDescription.js";
+import { menu } from "./menu.js";
+
+menu();
+initScrollReveal();
+typeWrite(document.querySelector(".typewriter"));
+
+hoverChangeExperience(
+  ".digitalhouse",
+  `As a Web Developer Intern at Nextupgrad Web Solution Pvt. Ltd., I worked on various web development projects under the guidance of experienced professionals. My responsibilities included assisting in front-end and back-end development tasks, collaborating with the team to create responsive and user-friendly websites, and gaining hands-on experience with web technologies and programming languages. Throughout my internship, I had the opportunity to contribute to real-world projects and enhance my web development skills.`,
+  "Web Developer Intern",
+  "Nextupgrad Web Solution Pvt. Ltd.",
+  "Aug 2023 - Present"
+);
+
+hoverChangeExperience(
+  ".zuplae",
+  `Part-time Financial Advisor, dedicated to assisting clients with tailored financial plans and generating leads for financial growth.`,
+  "Financial Advisor(Part Time)",
+  "Niftel Communication.",
+  "May 2023 - Sep 2023"
+);
+
+hoverChangeExperience(
+  ".codigofontetv",
+  `Remote Freelance Content Creator, passionate about producing compelling and relevant content across various platforms to captivate audiences and drive engagement.`,
+  "Content Creator",
+  "Freelancing",
+  "Remote"
+);
+
+hoverChangeExperience(
+  ".contweb",
+  `Remote Freelance Soft Skill Trainer, empowering individuals and teams to enhance their interpersonal skills for success.`,
+  "Soft Skill Trainer",
+  "Freelancing",
+  "Remote"
+);
+
+hoverChangeDescription(
+  ".html",
+  "HTML: I'm the storyteller of the digital realm, wielding Hypertext Markup Language to craft the foundation of captivating online tales, where every element plays a role in the grand narrative of user experience."
+);
+hoverChangeDescription(
+  ".css",
+  "CSS: I'm the cinematic stylist of the web world, using Cascading Style Sheets to paint digital canvases that not only look stunning but also dance to the rhythm of responsiveness."
+);
+hoverChangeDescription(
+  ".js",
+  "JS: I'm the digital magician, using JavaScript to bring interactivity and life to static web pages, turning them into dynamic and engaging experiences that leave users spellbound."
+);
+// hoverChangeDescription(
+//   ".sass",
+//   "Sass é um pré-processador CSS que adiciona alguns recursos que não estão disponíveis de forma nativa."
+// );
+hoverChangeDescription(
+  ".react",
+  "React JS: I'm the director of the web stage, employing React to orchestrate captivating user interfaces that seamlessly adapt to the audience's every cue, creating web experiences that steal the show."
+);
+hoverChangeDescription(
+  ".next",
+  "Next.js: I'm the architect of the web's future, harnessing the power of Next.js to build lightning-fast and SEO-friendly websites that lead the way in the digital landscape."
+);
+// hoverChangeDescription(
+//   ".styled",
+//   "styled-components é uma biblioteca que utiliza o conceito de CSS-in-JS, ou seja, que nos permite escrever códigos CSS dentro do Javascript."
+// );
+hoverChangeDescription(
+  ".tailwind",
+  "Tailwind CSS: I'm the design virtuoso, using Tailwind CSS to sculpt elegant and efficient user interfaces with precision and speed, bringing harmony to the digital canvas."
+);
+hoverChangeDescription(
+  ".typescript",
+  "TypeScript: I'm the script doctor, using TypeScript to fortify your codebase with static typing, ensuring a healthy and robust application that stands strong in the face of complexity."
+);
+// hoverChangeDescription(
+//   ".radix",
+//   "Radix é uma biblioteca que fornece componentes acessíveis e sem estilos para a criação de aplicação em React."
+// );
+// hoverChangeDescription(
+//   ".cypress",
+//   "Cypress é um framework para automação de testes end-to-end, onde atualmente usa a linguagem JavaScript."
+// );
+// hoverChangeDescription(
+//   ".storybook",
+//   "Storybook é uma ferramenta que tem como objetivo documentar e testar os componentes da aplicação."
+// );
